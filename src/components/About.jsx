a5517259528b3637ab3890de91cb6abdb62d6d40
Kenneth Basjan <92210924+Abbbtle@users.noreply.github.com>
2023-08-22 13:24:45 +0200

    Updated index.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function About() {
+
+const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className='about mx-10 mt-10'>
+      <div className='mx-10 mt-10'>
+        <h2 className='text-5xl font-semibold'>Meet Kenneth Basjan</h2>
+        <h4 className='text-3xl font-semibold'>Your Web Development Partner</h4>
+      </div>
+    </div>
+  );
+};
 
 export default About
