a4b25e811761a6dd3ede20e98abfff31760e28e6
Kenneth Basjan <92210924+Abbbtle@users.noreply.github.com>
2023-08-23 11:46:56 +0200

    Finished Work introduction

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,12 +5,12 @@ const Services = () => {
   return (
     <div>
       {/* Services Heading */}
-      <h2 className='text-[72px] font-semibold text-white mt-10 uppercase text-center'>
+      <h2 className='text-[72px] font-bold text-white mt-10 uppercase text-center'>
           What I do <span className='text-[#906EF0]'>best</span>
           <div className='mr-[405px] ml-[405px] mt-[-40px]'><HorizontalRule/></div>
       </h2>
-      <p className='font-semibold text-center text-[20px]'>
-      Discover an Array of <span className='text-[#906EF0]'>Expert Services Customized</span> for Your <span className='text-[#906EF0]'>Needs</span>
+      <p className='font-medium text-center text-[20px]'>
+        Discover an array of <span className='text-[#906EF0]'>expert services customized</span> for your <span className='text-[#906EF0]'>needs</span>
       </p>
     </div>
   );
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import HorizontalRule from './HorizontalRule'
 
-function Work() {
+const Work = () => {
   return (
-    <div>Work</div>
-  )
-}
+    <div>
+      {/* Work Heading */}
+      <h2 className='text-[72px] font-bold text-white uppercase text-center mt-10 mb-[-40px]'>
+        Exploring
+      </h2>
+      <h2 className='text-[72px] font-bold text-[#906EF0] uppercase text-center'>
+        Past Creations
+        <div className='mr-[390px] ml-[390px] mt-[-40px]'><HorizontalRule/></div>
+      </h2>
+      <p className='font-medium text-center text-[20px] mr-20 ml-20'>
+        Take a glimpse into a few of my past projects, where <span className='text-[#906EF0]'>creativity meets functionality </span>  
+        in a seamless dance. Each website represents a <span className='text-[#906EF0]'>unique challenge </span>
+        that I <span className='text-[#906EF0]'>embraced with enthusiasm</span>, ensuring that every line of code and 
+        pixel was meticulously curated. As you browse through these <span className='text-[#906EF0]'>creations</span>, 
+        you'll witness my <span className='text-[#906EF0]'>commitment to excellence</span> and an <span className='text-[#906EF0]'>unwavering dedication </span> 
+        to delivering digital solutions that leave a <span className='text-[#906EF0]'>lasting impact</span>.
+      </p>
+    </div>
+  );
+};
 
 export default Work
