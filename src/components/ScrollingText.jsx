88e3b3f80427c4d1708e46b074bfb66009c9535e
Kenneth Basjan <92210924+Abbbtle@users.noreply.github.com>
2023-08-24 11:10:36 +0200

    Finished scrolling text

--- src/components/ScrollingText.jsx
+++ src/components/ScrollingText.jsx
@@ -1,18 +1,99 @@
-import React, { useRef, useEffect } from "react";
-import '../index.css'
+import React from "react";
 
 const ScrollingText = () => {
   return (
   <div>
 
-    <div class="overflow-x-hidden">
-      <div class="py-12 animate-marquee whitespace-nowrap ">
-        <span class="mx-4 text-[200px] uppercase">Make the best decision for your business</span>
+    <div className="overflow-x-hidden">
+      <div className="py-12 animate-marquee whitespace-nowrap ">
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className='text-[#906EF0]'>Make</span> the <span className='text-[#906EF0]'>best</span> decision <span className='text-[#906EF0]'>for</span> your <span className='text-[#906EF0]'>business</span></span>
+        <span className="mx-4 text-[200px] font-bold uppercase">Make <span className='text-[#906EF0]'>the</span> best <span className='text-[#906EF0]'>decision</span> for <span className='text-[#906EF0]'>your</span> business</span>
+        
       </div>
     </div>
 
-    <div class="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
-      <span class="mx-4 text-[200px] uppercase">Make the best decision for your business</span>
+    <div className="overflow-x-hidden">
+      <div className="py-12 animate-marquee whitespace-nowrap">
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        <span className="mx-4 text-[200px] font-bold uppercase"><span className="text-[#906EF0]">Contact</span> Me</span>
+        
+      </div>
     </div>
   </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
   theme: {
 
     animation: {
-      marquee: 'marquee 10s linear infinite',
+      marquee: 'marquee 300s linear infinite',
     },
     keyframes: {
       marquee: {
         '0%': { transform: 'translateX(0%)' },
-        '100%': { transform: 'translateX(-100%)' },
+        '100%': { transform: 'translateX(-2500%)' },
       },
       marquee2: {
         '0%': { transform: 'translateX(100%)' },
