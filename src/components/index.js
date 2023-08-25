ace649e68a7e96289e2b3870f49520c08e760fb9
Kenneth Basjan <92210924+Abbbtle@users.noreply.github.com>
2023-08-25 10:30:50 +0200

    removed dummy contact2 component

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,6 @@ import Flow from "./Flow";
 import Testimonials from "./Testimonials";
 import ScrollingText from "./ScrollingText";
 import Contact from "./Contact";
-import Contact2 from "./Contact2";
 import Footer from "./Footer";
 import CustomCursor from "./CustomCursor";
 import "../index.css"
@@ -24,7 +23,6 @@ export {
     Testimonials,
     ScrollingText,
     Contact,
-    Contact2,
     Footer,
     CustomCursor
 }
