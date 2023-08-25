e0a1edd1de8b8cdeb3a2526f8b7cd4f579b74a80
Kenneth Basjan <92210924+Abbbtle@users.noreply.github.com>
2023-08-25 16:58:04 +0200

    Started with slider for tech stack section, will continue later

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import logo from './Images/Logos/logo.png';
 import kenneth1 from './Images/Kenneth/kenneth-younglings.png'
 import presentation from './Images/Kenneth/presentation-edited.jpg'
+import react from './Images/Logos/react.png'
 
 export {
     logo,
     kenneth1,
-    presentation
+    presentation,
+    react
 };
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,18 +1,60 @@
 import React, { useState } from "react";
 import {FaReact} from 'react-icons/fa';
+import {SiTailwindcss} from 'react-icons/si';
+import {FaHtml5} from 'react-icons/fa';
+import {SiCss3} from 'react-icons/si';
+import {IoLogoJavascript} from 'react-icons/io'
+import {BsGit} from 'react-icons/bs'
+import {CgFigma} from 'react-icons/cg'
+import {react} from '../assets'
 
 const techStackData = [
   {
     name: "Tailwind CSS",
     category: "Styling",
-    icon: {FaReact}, // Replace with actual image path
+    icon: {SiTailwindcss},
   },
   {
     name: "ReactJS",
     category: "Web Framework",
-    icon: "react.png", // Replace with actual image path
+    image: {react},
   },
-  // Add data for other software in your tech stack
+  {
+    name: "Figma",
+    category: "UI/UX",
+    icon: {CgFigma},
+  },
+  {
+    name: "HTML",
+    category: "Web Framework",
+    icon: {FaHtml5},
+  },
+  {
+    name: "CSS",
+    category: "Web Framework",
+    icon: {SiCss3},
+  },
+  {
+    name: "JavaScript",
+    category: "Web Framework",
+    icon: {IoLogoJavascript},
+  },
+  {
+    name: "Git",
+    category: "Web Framework",
+    icon: {BsGit},
+  },
+  {
+    name: "WordPress",
+    category: "Web Framework",
+    icon: {BsGit},
+  },
+  {
+    name: "Elementor",
+    category: "Web Framework",
+    icon: {BsGit},
+  },
+  
 ];
 
 const Slider = () => {
@@ -29,33 +71,40 @@ const Slider = () => {
   return (
     <div className="flex items-center justify-center py-8">
       <div className="flex space-x-8">
+
+        {/* Arrow Left */}
         <button
-          className="text-gray-500 hover:text-gray-700"
+          className="text-white hover:text-[#906EF0]"
           onClick={handlePrev}
         >
           &lt;
         </button>
+
+        {/* Cards */}
         {techStackData.slice(currentIndex, currentIndex + 3).map((software, index) => (
-          <div key={index} className="w-64 bg-white shadow-md rounded p-4">
+          <div key={index} className="w-64 bg-[#0D0D0D] shadow-md rounded p-4">
             <img
-              src={software.icon}
+              src={software.image}
               alt={software.name}
               className="w-32 mx-auto mb-4"
             />
             <div className="flex justify-between">
               <p className="text-lg font-semibold">{software.name}</p>
-              <div className="rounded-full bg-blue-500 text-white px-2 py-1">
+              <div className="rounded-full bg-transparent border-gray-400 text-[#808080] px-2 py-1">
                 {software.category}
               </div>
             </div>
           </div>
         ))}
+
+        {/* Arrow Right */}
         <button
-          className="text-gray-500 hover:text-gray-700"
+          className="text-white hover:text-[#906EF0]"
           onClick={handleNext}
         >
           &gt;
         </button>
+        
       </div>
     </div>
   );
