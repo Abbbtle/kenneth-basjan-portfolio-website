c6960944801a0bab7d2f3cf8f835731ed6c24759
Kenneth Basjan <92210924+Abbbtle@users.noreply.github.com>
2023-08-28 14:03:44 +0200

    Slider now infinitely scroll depending on which button is pressed

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,111 +5,111 @@ const techStackData = [
   {
     name: "Tailwind CSS",
     category: "Styling",
-    icon: {tailwind},
+    image: {tailwind},
   },
   {
     name: "ReactJS",
     category: "Web Framework",
-    icon: {react},
+    image: {react},
   },
   {
     name: "Github",
     category: "Version Control",
-    icon: {github},
+    image: {github},
   },
   {
     name: "Git",
     category: "Version Control",
-    icon: {git},
+    image: {git},
   },
   {
     name: "WordPress",
     category: "CMS",
-    icon: {wordpress},
+    image: {wordpress},
   },
   {
     name: "Figma",
     category: "UI/UX",
-    icon: {figma},
+    image: {figma},
   },
   {
     name: "HTML",
     category: "Hypertext Markup Language",
-    icon: {html5},
+    image: {html5},
   },
   {
     name: "CSS",
     category: "Cascading Style Sheets.",
-    icon: {css3},
+    image: {css3},
   },
   {
     name: "Javascript",
     category: "Programming Language",
-    icon: {javascript},
+    image: {javascript},
   },
   {
     name: "Vite",
     category: "Build Tool",
-    icon: {vite},
+    image: {vite},
   },
   {
     name: "VS Code",
     category: "Code Editor",
-    icon: {vscode},
+    image: {vscode},
   }
 ];
 
 const Slider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + techStackData.length) % techStackData.length);
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % techStackData.length);
-  };
-
-  return (
-    <div className="flex items-center justify-center py-8">
-      <div className="flex space-x-8">
-
-        {/* Arrow Left */}
-        <button
-          className="text-white hover:text-[#906EF0]"
-          onClick={handlePrev}
-        >
-          &lt;
-        </button>
-
-        {/* Cards */}
-        {techStackData.slice(currentIndex, currentIndex + 3).map((software, index) => (
-          <div key={index} className="w-64 bg-[#0D0D0D] shadow-md rounded p-4">
-            <img
-              src={software.icon}
-              alt={software.name}
-              className="w-32 mx-auto mb-4"
-            />
-            <div className="flex justify-between">
-              <p className="text-lg font-semibold">{software.name}</p>
-              <div className="rounded-full bg-transparent border-gray-400 text-[#808080] px-2 py-1">
-                {software.category}
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    const handlePrev = () => {
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + techStackData.length) % techStackData.length);
+    };
+  
+    const handleNext = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % techStackData.length);
+    };
+  
+    const displayData = [];
+    let counter = 0;
+    while (displayData.length < 3) {
+      displayData.push(techStackData[(currentIndex + counter) % techStackData.length]);
+      counter++;
+    }
+  
+    return (
+      <div className="flex items-center justify-center py-8">
+        <div className="flex space-x-8">
+          <button
+            className="text-white hover:text-[#906EF0]"
+            onClick={handlePrev}
+          >
+            &lt;
+          </button>
+          {displayData.map((software, index) => (
+            <div key={index} className="w-72 flex flex-col justify-end bg-[#0D0D0D] shadow-2xl shadow-black rounded-3xl border-[#808080] border-[1px] p-4">
+              <img
+                src={software.image}
+                alt={software.name}
+                className="w-64 h-auto mx-16 mb-4"
+              />
+              <div className="flex justify-between">
+                <p className="text-lg font-semibold">{software.name}</p>
+                <div className="rounded-full bg-transparent border-gray-400 text-[#808080] px-2 py-1">
+                  {software.category}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-
-        {/* Arrow Right */}
-        <button
-          className="text-white hover:text-[#906EF0]"
-          onClick={handleNext}
-        >
-          &gt;
-        </button>
-        
+          ))}
+          <button
+            className="text-white hover:text-[#906EF0]"
+            onClick={handleNext}
+          >
+            &gt;
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Slider;
+    );
+  };
+  
+  export default Slider;
