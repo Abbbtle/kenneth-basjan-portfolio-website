d34b49587022bd1f2c8f1e0b4ce0f54b0e902f2f
Kenneth Basjan <92210924+Abbbtle@users.noreply.github.com>
2023-09-01 15:57:34 +0200

    Almost done with the first past project cards. Added all relevant information. Left with alignment fixes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import HorizontalRule from './HorizontalRule'
 import jam from '../assets/Images/Screenshots/jam-blog.png'
 import billiard from '../assets/Images/Screenshots/thebilliardman.png'
+import {LuStars} from 'react-icons/lu'
+import Button1 from './Button1'
+import jam_favicon from '../assets/Images/Favicon/jam-favicon.webp'
+import tbm_favicon from '../assets/Images/Favicon/billiard-man-logo.png'
 
 const Work = () => {
   return (
@@ -26,8 +30,8 @@ const Work = () => {
       {/* Past Work Cards */}
       <div>
         {/* Card 1 */}
-        <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-          <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-2">
+          <div className="border-[1px] border-[#808080] relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
             <svg
               viewBox="0 0 1024 1024"
               className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
@@ -42,31 +46,33 @@ const Work = () => {
               </defs>
             </svg>
             <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Boost your productivity.
-                <br />
-                Start using our app today.
+              <div className='bg-[#795a5a] mb-5 rounded-[10px] w-14 h-14 items-center flex justify-center shadow-inner shadow-[#d1afaf]'>
+                  <img 
+                    src= {jam_favicon} 
+                    alt="JAM Favicon" 
+                    width={50}
+                    height={50}
+                  />
+              </div>
+              <h2 className="text-[30px] font-bold text-white sm:text-4xl">
+              JAM Consulting –– Business Consulting, Management, & Startup Coaching
               </h2>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
-                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+              <p className="mt-6 text-lg leading-8 text-gray-100">
+                At JAM Consulting, I took charge of creating their dynamic website, crafting a seamless digital 
+                presence to complement their business consulting, management, and startup coaching services.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                <a
-                  href="#"
-                  className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                >
-                  Get started
-                </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-white">
-                  Learn more <span aria-hidden="true">→</span>
+                
+                <a href="#" className="text-lg font-normal leading-6 text-white">
+                  Go To Website <span aria-hidden="true">→</span>
                 </a>
               </div>
             </div>
             <div className="relative mt-16 h-80 lg:mt-8">
               <img
-                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+                className="absolute left-0 top-0 w-[60rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
                 src= {jam}
-                alt="Kenneth's Home Screen"
+                alt="JAM Consulting Home Screen"
                 width={1824}
                 height={1080}
               />
@@ -76,7 +82,7 @@ const Work = () => {
 
         {/* Card 2 */}
         <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-          <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <div className="border-[1px] border-[#808080] relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
             <svg
               viewBox="0 0 1024 1024"
               className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
@@ -115,13 +121,29 @@ const Work = () => {
               <img
                 className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
                 src= {billiard}
-                alt="Kenneth's Home Screen"
+                alt="The Billiard Man Home Screen"
                 width={1824}
                 height={1080}
               />
             </div>
           </div>
         </div>
+
+        {/* Card 3 */}
+        <div className='mx-auto max-w-6xl py-24 sm:px-6 sm:py-32 lg:px-8'>
+          <div className='bg-[#181818] shadow-lg shadow-black rounded-3xl border-dashed border-[1px] border-[#808080]'>
+            <div className='flex flex-col items-center mt-28 mb-28 justify-center'>
+              <LuStars className='text-[70px]'/>
+              <h3 className='uppercase font-bold text-[40px] mt-10'>Your project goes here</h3>
+              <p className='text-[20px] font-light'>Let's turn your idea into reality</p>
+              <div className='mt-20'>
+                <Button1>GET IN TOUCH</Button1>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+
       </div>
     </div>
   );
