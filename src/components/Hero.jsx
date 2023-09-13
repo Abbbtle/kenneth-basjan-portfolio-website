2419084c2366d35779fa5a8fb3a8427c276a76c3
Kenneth Basjan <92210924+Abbbtle@users.noreply.github.com>
2023-09-13 17:41:09 +0200

    minor fixes to hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,11 @@ const Hero = () => {
           Welcome to the World of <span className='text-[#906EF0]'>Innovation</span> and <span className='text-[#906EF0]'>Functionality</span>
         </h3>
       </div>
-      <div className='mx-10 sm:mx-5'>
+      <div className='mx-5 md:mx-8'>
         <HorizontalRule />
       </div>
       <div className='flex justify-between flex-col md:flex-row mx-10 items-center'>
-        <p className='text-[20px] sm:text-center md:text-left sm:text-[15px] md:w-3/4 font-semibold'>
+        <p className='text-[20px] sm:text-center md:text-left sm:text-sm md:w-3/4 font-semibold'>
           Are you looking for a <span className='text-[#906EF0]'>dynamic</span> and <span className='text-[#906EF0]'>cutting-edge web presence</span>? Look no further! <br />
           I bring your <span className='text-[#906EF0]'>ideas to life</span> on the <span className='text-[#906EF0]'>digital canvas</span>. Let's <span className='text-[#906EF0]'>collaborate</span> and <span className='text-[#906EF0]'>build</span> <br />
           something <span className='text-[#906EF0]'>extraordinary together</span>!
